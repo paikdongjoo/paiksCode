acac271d223bcfb6ce168d8ed243f07c36827ae2
paikdongjoo <paikdongjuice@gmail.com>
2020-06-25 20:17:19 +0900

    REST API를 통해 가게의 전체 정보와 상세 정보 전달 의존성 주입을 통한 interface와 imple 분리

--- src/test/java/kr/co/fastcampus/eatgo/interfaces/RestaurantControllerTest.java
+++ src/test/java/kr/co/fastcampus/eatgo/interfaces/RestaurantControllerTest.java
@@ -1,9 +1,11 @@
 package kr.co.fastcampus.eatgo.interfaces;
 
+import kr.co.fastcampus.eatgo.domain.RestaurantRepositoryImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -19,6 +21,9 @@ class RestaurantControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @SpyBean(RestaurantRepositoryImpl.class)
+    private RestaurantRepositoryImpl restaurantRepositoryImpl;
+
     @Test
     public void list() throws Exception {
         mvc.perform(get("/restaurants"))
@@ -26,6 +31,20 @@ class RestaurantControllerTest {
                 .andExpect(content().string(containsString("\"id\":1004")))
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("\"name\":\"Bob zip\"")));
+    }
+
+    @Test
+    public void detail() throws Exception {
+        mvc.perform(get("/restaurants/1004"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("\"id\":1004")))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("\"name\":\"Bob zip\"")));
+        mvc.perform(get("/restaurants/2020"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("\"id\":2020")))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("\"name\":\"Cyber Food\"")));
     }
 
 }
