5b6cffc881b01420a8b4cb30b83a3dbe8c4727fd
paikdongjoo <paikdongjuice@gmail.com>
2020-06-26 13:47:36 +0900

    Mock 어노테이션을 활용한 가짜 객체 생성

--- src/test/java/kr/co/fastcampus/eatgo/application/RestaurantServiceTest.java
+++ src/test/java/kr/co/fastcampus/eatgo/application/RestaurantServiceTest.java
@@ -3,26 +3,46 @@ package kr.co.fastcampus.eatgo.application;
 import kr.co.fastcampus.eatgo.domain.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.BDDMockito.given;
 
 class RestaurantServiceTest {
 
     private RestaurantService restaurantService;
+    @Mock
     private RestaurantRepository restaurantRepository;
+    @Mock
     private MenuItemRepository menuItemRepository;
 
 
     @BeforeEach // Juni4 에서는 @Before
     // 모든 테스트가 실행하기 이전에 실행되게끔 하는 어노테이션
     public void setUp(){
-        RestaurantRepository restaurantRepository = new RestaurantRepositoryImpl();
-        menuItemRepository = new MenuItemRepositoryImpl();
-
+        MockitoAnnotations.initMocks(this);
+        mockRestaurantRepository();
+        mockMenuItemRepository();
         restaurantService = new RestaurantService(restaurantRepository, menuItemRepository);
+    }
+
+    private void mockMenuItemRepository() {
+        List<MenuItem> menuItems = new ArrayList<>();
+        menuItems.add(new MenuItem("Kimchi"));
+        given(menuItemRepository.findAllByRestaurantId(1004L)).willReturn(menuItems);
+    }
+
+    private void mockRestaurantRepository() {
+        List<Restaurant> restaurants = new ArrayList<>();
+        Restaurant restaurant = new Restaurant(1004L, "Bob zip", "Seoul");
+        restaurants.add(restaurant);
+        given(restaurantRepository.findAll()).willReturn(restaurants);
+        given(restaurantRepository.findById(1004L)).willReturn(restaurant);
     }
 
     @Test
@@ -40,7 +60,5 @@ class RestaurantServiceTest {
         MenuItem menuItem = restaurant.getMenuItems().get(0);
         assertThat(menuItem.getName(), is("Kimchi"));
     }
-
-
 
 }
--- src/test/java/kr/co/fastcampus/eatgo/interfaces/RestaurantControllerTest.java
+++ src/test/java/kr/co/fastcampus/eatgo/interfaces/RestaurantControllerTest.java
@@ -1,18 +1,21 @@
 package kr.co.fastcampus.eatgo.interfaces;
 
 import kr.co.fastcampus.eatgo.application.RestaurantService;
-import kr.co.fastcampus.eatgo.domain.MenuItemRepository;
-import kr.co.fastcampus.eatgo.domain.MenuItemRepositoryImpl;
-import kr.co.fastcampus.eatgo.domain.RestaurantRepositoryImpl;
+import kr.co.fastcampus.eatgo.domain.MenuItem;
+import kr.co.fastcampus.eatgo.domain.Restaurant;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.core.StringContains.containsString;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -24,34 +27,37 @@ class RestaurantControllerTest {
     @Autowired
     private MockMvc mvc;
 
-    @SpyBean(RestaurantService.class)
+    @MockBean
     private RestaurantService restaurantService;
-
-    @SpyBean(RestaurantRepositoryImpl.class)
-    private RestaurantRepositoryImpl restaurantRepositoryImpl;
-
-    @SpyBean(MenuItemRepositoryImpl.class)
-    private MenuItemRepository menuItemRepository;
 
     @Test
     public void list() throws Exception {
+        List<Restaurant> restaurants = new ArrayList<>();
+        restaurants.add(new Restaurant(1004L, "JOKER House", "Seoul"));
+        given(restaurantService.getRestaurants()).willReturn(restaurants);
         mvc.perform(get("/restaurants"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("\"id\":1004")))
                 .andExpect(status().isOk())
-                .andExpect(content().string(containsString("\"name\":\"Bob zip\"")))
-                .andExpect(content().string(
-                        containsString("Kimchi")
-                ));
+                .andExpect(content().string(containsString("\"name\":\"JOKER House\"")));
+                /**/
     }
 
     @Test
     public void detail() throws Exception {
+        Restaurant restaurant1 = new Restaurant(1004L, "JOKER House", "Seoul");
+        Restaurant restaurant2 = new Restaurant(2020L, "Cyber Food", "Seoul");
+        restaurant1.addMenuItem(new MenuItem("Kimchi"));
+        given(restaurantService.getRestaurant(1004L)).willReturn((Restaurant) restaurant1);
+        given(restaurantService.getRestaurant(2020L)).willReturn((Restaurant) restaurant2);
         mvc.perform(get("/restaurants/1004"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("\"id\":1004")))
                 .andExpect(status().isOk())
-                .andExpect(content().string(containsString("\"name\":\"Bob zip\"")));
+                .andExpect(content().string(containsString("\"name\":\"JOKER House\"")))
+                .andExpect(content().string(
+                        containsString("Kimchi")
+                ));
         mvc.perform(get("/restaurants/2020"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("\"id\":2020")))
